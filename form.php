8d54459c89795f12d09d88daa7ea35ab9d852d9f
Justin Carl de Guia <justincarlcdeguia@iskolarngbayan.pup.edu.ph>
2023-11-04 20:44:30 +0800

    Finished Register Page

--- form.php
+++ form.php
@@ -11,7 +11,7 @@
     <div class="registration-form">
     <h1 class="registration-form-title">Registration Form</h1>
     <form action="receiveData.php" method="post">
-        <label for="">Name: </label>
+        <!-- <label for="">Name: </label> -->
         <!-- <label for="fname">First Name: </label>
         <input type="text" name="fname" id="fname" placeholder="Juan Shanks" required> -->
         
@@ -21,25 +21,96 @@
         <!-- <label for="lname">Last Name: </label>
         <input type="text" name="lname" id="lname" placeholder="Dela Cruz" required> <br> -->
 
-        <input type="text" name="fname" id="fname" placeholder="First Name" required>
+        <!-- <input type="text" name="fname" id="fname" placeholder="First Name" required>
         <input type="text" name="minitial" id="minitial" placeholder="Middle Initial">
-        <input type="text" name="lname" id="lname" placeholder="Last Name" required> <br>
-        
+        <input type="text" name="lname" id="lname" placeholder="Last Name" required> <br> -->
+
+        <div class="form-group">
+        <label for="fname">First Name:</label>
+        <input type="text" name="fname" id="fname" placeholder="First Name" required>
+        </div>
+
+        <div class="form-group">
+        <label for="minitial">Middle Initial:</label>
+        <input type="text" name="minitial" id="minitial" placeholder="Middle Initial">
+        </div>
+
+        <div class="form-group">
+        <label for="lname">Last Name:</label>
+        <input type="text" name="lname" id="lname" placeholder="Last Name" required>
+        </div>
+
+        <!-- <div class="form-group">
         <label for="birthday">Birthdate: </label>
         <input type="date" name="birthdate" id="birthdate">
         <input type="number" name="month" id="month" hidden>
         <input type="number" name="day" id="day" hidden>
         <input type="number" name="year" id="year" hidden> <br>
+        </div>
+        
         <label for="age">Age:</label>
-        <input type="number" name="age" id="age" readonly> <br>
-        <label for="gender">Gender:</label> <br>
+        <input type="number" name="age" id="age" readonly> <br> -->
+
+        <div class="form-group">
+        <label for="birthday">Birthdate: </label>
+        <input type="date" name="birthdate" id="birthdate">
+        <input type="number" name="month" id="month" hidden>
+        <input type="number" name="day" id="day" hidden>
+        <input type="number" name="year" id="year" hidden> <br>
+        </div>
+
+        <div class="form-group">
+        <label for="age">Age:</label>
+        <input type="number" name="age" id="age" readonly>
+        </div>
+
+        <!-- <div class="form-group">
+        <div class="column">
+        <label for="birthday">Birthdate: </label>
+        <div class="form-input">
+        <input type="date" name="birthdate" id="birthdate">
+        <input type="number" name="month" id="month" hidden>
+        <input type="number" name="day" id="day" hidden>
+        <input type="number" name="year" id="year" hidden> <br>
+        </div>
+        </div>
+        <div class="form-input">
+        <div class="column">
+        <label for="age">Age:</label>
+        <input type="number" name="age" id="age" readonly>
+        </div>
+        </div>
+        </div>   -->
+
+
+        <!-- <label for="gender">Gender:</label> <br>
         <input type="radio" name="gender" id="male" value="Male" required>
         <label for="male">Male</label> <br>
         <input type="radio" name="gender" id="female" value="Female" required>
         <label for="female">Female</label> <br>
         <input type="radio" name="gender" id="others" value="Others" required>
         <label for="others">Others</label> <br>
-        <input type="text" name="gender" id="gender" disabled> <br>
+        <input type="text" name="gender" id="gender" disabled> <br> -->
+
+        <div class="form-group">
+        <label for="gender">Gender:</label> <nobr>
+        <div class="gender-options">
+        <label for="male"><input type="radio" name="gender" id="male" value="Male" required> Male</label>
+        <label for="female"><input type="radio" name="gender" id="female" value="Female" required> Female</label>
+        <label for="others"><input type="radio" name="gender" id="others" value="Others" required> Others</label>
+        <input type="text" name="gender" id="gender" disabled>
+        </div>
+        </div>
+<!-- 
+        <label for="gender">Gender:</label> <nobr>
+        <div class="gender-options">
+        <label for="male"><input type="radio" name="gender" id="male" value="Male" required> Male</label>
+        <label for="female"><input type="radio" name="gender" id="female" value="Female" required> Female</label>
+        <label for="others"><input type="radio" name="gender" id="others" value="Others" required> Others</label>
+        <input type="text" name="gender" id="gender" disabled>
+        </div> -->
+
+        <div class="form-group">
         <label for="region">Region:</label>
         <select name="region" id="region" required>
             <option value="">--- Choose Your Region ---</option>
@@ -61,17 +132,34 @@
             <option value="Cordillera Administrative Region">CAR - Cordillera Administrative Region</option>
             <option value="Bangsamoro Autonomous Region in Muslim Mindanao">BARMM - Bangsamoro Autonomous Region in Muslim Mindanao</option>
         </select> <br>
+        </div>
+
+        <div class="form-group">
         <label for="phone">Phone Number (+63): </label>
         <input type="number" name="phone" id="phone" maxlength="999999999" required> <br>
+        </div>
+
+        <div class="form-group">
         <label for="email">Email Address: </label>
         <input type="email" name="email" id="email" required>
         <br><br>
+        </div>
+
+        <div class="form-group">
         <label for="username">Username: </label>
         <input type="text" name="username" id="username" placeholder="Username"> <br>
+        </div>
+
+        <div class="form-group">
         <label for="password">Password: </label>
         <input type="password" name="password" id="password" placeholder="Password"> <br>
+        </div>
+
+        <div class="form-group">
         <label for="confirmpass">Confirm Password: </label>
         <input type="password" name="confirmpass" id="confirmpass" placeholder="Confirm Password"> <br>
+        </div>
+
         </div>
 
         <!-- <button type="button" id="checkerBtn">
